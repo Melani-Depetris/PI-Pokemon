95a16ce5afaa7439927e225e14ec0199f6795470
melani-depetris <melani.md98@gmail.com>
2023-08-27 20:44:16 -0300

    Traigo en este controller los types de pokemons en un array

--- api/src/controllers/types.js
+++ api/src/controllers/types.js
@@ -1,16 +1,23 @@
 const URL_BASE_TYPES = 'https://pokeapi.co/api/v2/type';
 const axios = require('axios')
+const Type = require('../models/Type')
 
-const types = async(req, res) => {
+const types = async (req, res) => {
     try {
-        const {results} = await axios.get(URL_BASE_TYPES).data
 
-        let allTypes= [];
-const typesPokemons = results.map(e => allTypes.push(e.name))
-console.log(allTypes);
-res.status(200).json(allTypes)
+        const { results } = (await axios.get(URL_BASE_TYPES)).data
+
+        const typesPokemon = results.map(e => e.name)
+
+        // const createTypes = await Type.bulkCreate(typesPokemon)
+
+        return typesPokemon ?
+            res.status(200).json(typesPokemon) :
+            res.status(400).send('Not found')
+
+
     } catch (error) {
-
+        res.status(500).send(error.message)
     }
 };
 module.exports = types;
