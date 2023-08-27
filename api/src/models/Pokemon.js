74441fc4a848fcfa2b668689162264544209a0b4
melani-depetris <melani.md98@gmail.com>
2023-08-27 18:29:53 -0300

    Modelo/tabla de los Pokemon para la db

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,49 @@
 const { DataTypes } = require('sequelize');
+const { v4: uuidv4 } = require('uuid');
+
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
+
   sequelize.define('pokemon', {
+
+    uuid: { //uuid
+      type: DataTypes.UUID,
+      defaultValue: sequelize.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
-  });
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    life: {
+     type: DataTypes.INTEGER,
+     allowNull: false
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, { timestamps: false });
 };
